150675b847cd000a72a118af4672b1cecd0b66f2
emartinez <enri.fc.98@gmail.com>
2023-01-07 19:27:34 +0100

    FIX: gnl only stopping on EOF

--- src/checker/instructions.c
+++ src/checker/instructions.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 16:20:03 by franmart          #+#    #+#             */
-/*   Updated: 2023/01/07 18:38:55 by franmart         ###   ########.fr       */
+/*   Updated: 2023/01/07 19:26:21 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,14 @@ int	exec_instructions(t_push_swap *ps)
 		return (0);
 	command = ft_gnl(STDIN_FILENO);
 	status = 0;
-	while (command != NULL && status == 0)
+	while (command != NULL)
 	{
-		status = parse_instructions(command, ps, ft_strlen(command));
 		if (status == 0)
-		{
-			free(command);
-			command = ft_gnl(STDIN_FILENO);
-		}
+			status = parse_instructions(command, ps, ft_strlen(command));
+		else
+			parse_instructions(command, ps, ft_strlen(command));
+		free(command);
+		command = ft_gnl(STDIN_FILENO);
 	}
 	return (status);
 }
